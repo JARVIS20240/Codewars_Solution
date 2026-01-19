7f7ae55ddc0bbb343ba80b38e43d292263836e29
JARVIS20240 <mistrykaran2911@gmail.com>
2026-01-19 21:50:26 +0530

    solve problame-2

--- Fundamental_practice/02_remove_string_spae.py
+++ Fundamental_practice/02_remove_string_spae.py
@@ -10,5 +10,9 @@ Examples (Input -> Output):
 
 def no_space(x):
     #your code here
+    new = x.replace(" ", "")
+    return new
     
-    pass
+print(no_space("8 j 8   mBliB8g  imjB8B8  jl  B"))
+print(no_space("8 8 Bi fk8h B 8 BB8B B B  B888 c hl8 BhB fd"))
+print(no_space("8aaaaa dddd r     "))
